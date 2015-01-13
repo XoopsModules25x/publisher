323cc082c224df87301c14486a8a619d3a45acda
Mamba (Michael Beck) <mambax7@gmail.com>
2015-01-13 16:40:47 -0500

    adding missing parenthesis

--- publisher/blocks/search.php
+++ publisher/blocks/search.php
@@ -44,9 +44,9 @@ function publisher_search_show($options)
     $username = XoopsRequest::getString('uname', XoopsRequest::getString('uname', null, 'GET'), 'POST');
     $searchin = XoopsRequest::getArray('searchin', (explode("|", XoopsRequest::getArray('searchin', array(), 'GET'))), 'POST');
     $sortby   = XoopsRequest::getString('sortby', XoopsRequest::getString('sortby', null, 'GET'), 'POST');
-    $term     = XoopsRequest::getString('term', XoopsRequest::getString('term', '', 'GET');
+    $term     = XoopsRequest::getString('term', XoopsRequest::getString('term', '', 'GET'));
 
-    //mb TODO simplify next lines with cetogory
+    //mb TODO simplify next lines with category
     $category = XoopsRequest::getArray('category', array(), 'POST') ? XoopsRequest::getArray('category', array(), 'POST') : (XoopsRequest::getArray('category', null, 'GET'));
     if (empty($category) || (is_array($category) && in_array("all", $category))) {
         $category = array();

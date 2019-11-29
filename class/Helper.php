1e634cf80ba52fae1a689c1b9838a20ab42c8eb6
mambax7 <mambax7@gmail.com>
2019-11-29 18:27:58 -0500

    __NAMESPACE__

--- class/Helper.php
+++ class/Helper.php
@@ -79,12 +79,16 @@ class Helper extends \Xmf\Module\Helper
     public function getHandler($name)
     {
         $ret   = false;
-        /** @var \XoopsMySQLDatabase $db */
-        $db    = \XoopsDatabaseFactory::getDatabaseConnection();
-        $helper = self::getInstance();
-        $class = __NAMESPACE__ . '\\' . ucfirst($name) . 'Handler';
-        $ret   = new $class($db, $helper);
 
+        $class =  __NAMESPACE__ . '\\' . $name . 'Handler';
+        if (!class_exists($class)) {
+            throw new \RuntimeException("Class '$class' not found");
+        }
+        /** @var \XoopsMySQLDatabase $db */
+        $db     = \XoopsDatabaseFactory::getDatabaseConnection();
+        $helper = self::getInstance();
+        $ret    = new $class($db, $helper);
+        $this->addLog("Getting handler '{$name}'");
         return $ret;
     }
 }

ca05cdf9137396b00c362a7b61c8799c0fb91dc2
zyspec <owners@zyspec.com>
2019-12-09 15:52:49 -0600

    Minor 'consistency' changes
    * changed $catID to $cat_id
    * updated phpDocs
    * removed plugin.tag.php in ./class/plugin directory. File was
    unnecessary duplicate of file in ./include directory.

--- include/plugin.tag.php
+++ include/plugin.tag.php
@@ -10,10 +10,9 @@
  */
 
 /**
+ * @package         XoopsModules\Publisher
  * @copyright       The XUUPS Project http://sourceforge.net/projects/xuups/
  * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
- * @package         Publisher
- * @subpackage      Include
  * @since           1.0
  * @author          trabis <lusopoemas@gmail.com>
  * @author          The SmartFactory <www.smartfactory.ca>
@@ -23,21 +22,27 @@ use XoopsModules\Publisher;
 
 // defined('XOOPS_ROOT_PATH') || die('Restricted access');
 
-/** Get item fields: title, content, time, link, uid, uname, tags *
+/** Get item fields: title, content, time, link, uid, tags
  *
- * @param $items
+ * @param array $items
  */
 function publisher_tag_iteminfo(&$items)
 {
+    if (empty($items) || !is_array($items)) {
+        return false;
+    }
+
     $items_id = [];
-    foreach (array_keys($items) as $catId) {
-        // Some handling here to build the link upon catid
-        // if catid is not used, just skip it
-        foreach (array_keys($items[$catId]) as $item_id) {
+    foreach (array_keys($items) as $cat_id) {
+        // Some handling here to build the link upon cat_id
+        // if cat_id is not used, just skip it
+        foreach (array_keys($items[$cat_id]) as $item_id) {
             // In article, the item_id is "art_id"
             $items_id[] = (int)$item_id;
         }
     }
+    $items_id = array_unique($items_id); // remove duplicate ids
+
     /** @var \XoopsModules\Publisher\Helper $helper */
     $helper = \XoopsModules\Publisher\Helper::getInstance();
     /** @var Publisher\ItemHandler $itemHandler */
@@ -45,21 +50,30 @@ function publisher_tag_iteminfo(&$items)
     $criteria    = new \Criteria('itemid', '(' . implode(', ', $items_id) . ')', 'IN');
     $items_obj   = $itemHandler->getObjects($criteria, 'itemid');
 
+    //make sure Tag module tag_parse_tag() can be found
+    if (!method_exists('XoopsModule\Tag\Utility', 'tag_parse_tag')) {
+        require_once $GLOBALS['xoops']->path('modules/tag/include/functions.php');
+        $parse_function = 'tag_parse_tag';
+    } else {
+        $parse_function = 'XoopsModule\Tag\Utility::tag_parse_tag';
+    }
+
     /** @var Publisher\Item $item_obj */
-    foreach (array_keys($items) as $catId) {
-        foreach (array_keys($items[$catId]) as $item_id) {
+    foreach (array_keys($items) as $cat_id) {
+        foreach (array_keys($items[$cat_id]) as $item_id) {
             $item_obj                = $items_obj[$item_id];
-            $items[$catId][$item_id] = [
+            $items[$cat_id][$item_id] = [
                 'title'   => $item_obj->getVar('title'),
                 'uid'     => $item_obj->getVar('uid'),
                 'link'    => "item.php?itemid={$item_id}",
                 'time'    => $item_obj->getVar('datesub'),
-                'tags'    => tag_parse_tag($item_obj->getVar('item_tag', 'n')), // optional
+                'tags'    => $parse_function($item_obj->getVar('item_tag', 'n')), // optional
                 'content' => '',
             ];
         }
     }
     unset($items_obj);
+    return true;
 }
 
 /** Remove orphan tag-item links *

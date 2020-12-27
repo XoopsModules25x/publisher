8475bacabaa76158af1215303dc9079f41cf0cc9
mambax7 <mambax7@gmail.com>
2020-12-27 02:46:45 -0500

    move editFile() to Utility

--- class/Utility.php
+++ class/Utility.php
@@ -220,6 +220,52 @@ class Utility extends Common\SysUtility
         //        unset($categoryObj);
     }
 
+
+    /**
+     * @param bool $showmenu
+     * @param int  $fileid
+     * @param int  $itemId
+     */
+    public static function editFile($showmenu = false, $fileid = 0, $itemId = 0)
+    {
+        $helper = Helper::getInstance();
+        require_once $GLOBALS['xoops']->path('class/xoopsformloader.php');
+
+        // if there is a parameter, and the id exists, retrieve data: we're editing a file
+        if (0 != $fileid) {
+            // Creating the File object
+            /** @var \XoopsModules\Publisher\File $fileObj */
+            $fileObj = $helper->getHandler('File')->get($fileid);
+
+            if ($fileObj->notLoaded()) {
+                redirect_header('<script>javascript:history.go(-1)</script>', 1, _AM_PUBLISHER_NOFILESELECTED);
+            }
+
+            echo "<br>\n";
+            echo "<span style='color: #2F5376; font-weight: bold; font-size: 16px; margin: 6px 6px 0 0; '>" . _AM_PUBLISHER_FILE_EDITING . '</span>';
+            echo '<span style="color: #567; margin: 3px 0 12px 0; font-size: small; display: block; ">' . _AM_PUBLISHER_FILE_EDITING_DSC . '</span>';
+            static::openCollapsableBar('editfile', 'editfileicon', _AM_PUBLISHER_FILE_INFORMATIONS);
+        } else {
+            // there's no parameter, so we're adding an item
+            $fileObj = $helper->getHandler('File')->create();
+            $fileObj->setVar('itemid', $itemId);
+            echo "<span style='color: #2F5376; font-weight: bold; font-size: 16px; margin: 6px 6px 0 0; '>" . _AM_PUBLISHER_FILE_ADDING . '</span>';
+            echo '<span style="color: #567; margin: 3px 0 12px 0; font-size: small; display: block; ">' . _AM_PUBLISHER_FILE_ADDING_DSC . '</span>';
+            static::openCollapsableBar('addfile', 'addfileicon', _AM_PUBLISHER_FILE_INFORMATIONS);
+        }
+
+        // FILES UPLOAD FORM
+        /** @var File $fileObj */
+        $uploadForm = $fileObj->getForm();
+        $uploadForm->display();
+
+        if (0 != $fileid) {
+            static::closeCollapsableBar('editfile', 'editfileicon');
+        } else {
+            static::closeCollapsableBar('addfile', 'addfileicon');
+        }
+    }
+
     /**
      * @param bool          $showmenu
      * @param int           $categoryId

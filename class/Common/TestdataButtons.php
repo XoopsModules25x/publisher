891cb7a4f3da6b3b0049274fe21815388b53010c
mambax7 <mambax7@gmail.com>
2022-02-07 10:47:32 -0500

    cosmetics

--- class/Common/TestdataButtons.php
+++ class/Common/TestdataButtons.php
@@ -29,21 +29,27 @@ use XoopsModules\Publisher\Helper;
  */
 class TestdataButtons
 {
-    //functions for import buttons
+    /** Button status constants */
+    private const SHOW_BUTTONS = 1;
+    private const HIDE_BUTTONS = 0;
+
     /**
-     * @param \Xmf\Module\Admin $adminObject
+     * Load the test button configuration
+     *
+     * @param Xmf\Module\Admin $adminObject
+     *
      * @return void
      */
-    public static function loadButtonConfig(\Xmf\Module\Admin $adminObject)
+    public static function loadButtonConfig(Xmf\Module\Admin $adminObject): void
     {
-        $moduleDirName       = \basename(\dirname(__DIR__, 2));
-        $moduleDirNameUpper  = \mb_strtoupper($moduleDirName);
-        $yamlFile            = \dirname(__DIR__, 2) . '/config/admin.yml';
-        $config[]              = Yaml::readWrapped($yamlFile); // work with phpmyadmin YAML dumps
-        $displaySampleButton = $config[0]['displaySampleButton'];
+        $moduleDirName      = \basename(\dirname(__DIR__, 2));
+        $moduleDirNameUpper = \mb_strtoupper($moduleDirName);
         $helper              = Helper::getInstance();
+        $yamlFile            = $helper->path('/config/admin.yml');
+        $config             = Yaml::readWrapped($yamlFile); // work with phpmyadmin YAML dumps
+        $displaySampleButton = $config['displaySampleButton'];
 
-        if (1 == $displaySampleButton) {
+        if (self::SHOW_BUTTONS == $displaySampleButton) {
             \xoops_loadLanguage('admin/modulesadmin', 'system');
             $adminObject->addItemButton(\constant('CO_' . $moduleDirNameUpper . '_' . 'LOAD_SAMPLEDATA'), $helper->url('testdata/index.php?op=load'), 'add');
             $adminObject->addItemButton(\constant('CO_' . $moduleDirNameUpper . '_' . 'SAVE_SAMPLEDATA'), $helper->url('testdata/index.php?op=save'), 'add');
@@ -56,20 +62,30 @@ class TestdataButtons
         }
     }
 
+    /**
+     * Hide the test buttons
+     *
+     * @return void
+     */
     public static function hideButtons(): void
     {
         $yamlFile                   = \dirname(__DIR__, 2) . '/config/admin.yml';
         $app                        = [];
-        $app['displaySampleButton'] = 0;
+        $app['displaySampleButton'] = self::HIDE_BUTTONS;
         Yaml::save($app, $yamlFile);
         \redirect_header('index.php', 0, '');
     }
 
+    /**
+     * Show the test buttons
+     *
+     * @return void
+     */
     public static function showButtons(): void
     {
         $yamlFile                   = \dirname(__DIR__, 2) . '/config/admin.yml';
         $app                        = [];
-        $app['displaySampleButton'] = 1;
+        $app['displaySampleButton'] = self::SHOW_BUTTONS;
         Yaml::save($app, $yamlFile);
         \redirect_header('index.php', 0, '');
     }
